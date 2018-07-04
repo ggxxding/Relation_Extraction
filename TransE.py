e765315836e4c608dc6348709881da8d89bf4ed9
ggxxding-1002-273 <ggxxding@163.com>
2018-07-04 17:08:14 +0800

    180704-1
    still writing

--- TransE.py
+++ TransE.py
@@ -2,17 +2,41 @@ import numpy as np
 from copy import deepcopy
 
 class TransE:
-	
 	def __init__(self, entityList, relationList, tripleList, margin = 1, learningRate = 0.00001, dim = 10, L1 = True):
 		self.margin=margin
 		self.learningRate=learningRate
-class Foo():
-	x=9
-	def __init__(self,x=1):
-		self.x=x
-	def fu(self,b):
-		self.x=b
+		self.dim=dim
+		self.L1=L1
+		self.entityList=entityList#初始化后，变为字典，key是entity，values是其向量（使用narray）。
+		self.relationList=relationList
+		self.tripleList=tripleList
+		self.loss=0
 
+	def initialize(self):
+		entityVectorList={}
+		relationVectorList={}
+		for entity in entityList:
+			n=0
+			entityVector=[]
+			while n<self.dim:
+				ram=init(self.dim)
+				entityVector.append(ram)
+				n+=1
+			entityVector=norm(entityVector)
+			entityVectorList[entity]=entityVector
+		print('实体向量初始化完成，共有%d个'%len(entityVectorList))
+		for relation in relationList:
+			n=0
+			relationVector=[]
+			while n<self.dim:
+				ram=init(self.dim)
+				relationVector.append(ram)
+				n+=1
+			relationVector=norm(relationVector)
+			relationVectorList[relation]=relationVector
+		print('关系向量初始化完成，共有%d个'%len(relationVectorList))
+		self.entityList=entityVectorList
+		self.relationList=relationVectorList
 
 
 def init(dim):
@@ -22,6 +46,7 @@ def init(dim):
 	return np.random.uniform(-6/(dim**0.5),6/(dim**0.5))
 
 def distanceL1(h,l,t):
+	#L1范数
 	s=h+l-t
 	sum=np.fabs(s).sum()
 	return sum
@@ -42,18 +67,18 @@ def norm(list):
 		list[i]=list[i]/var
 		i=i+1
 	return np.array(list)
-
-def openDetailsAndId(dir,sp=','):
+#返回行数和指定
+def openDetailsAndId(dir,sp=',',col=0):
 	idNum=0
 	list=[]
 	with open(dir) as file:
 		lines=file.readlines()
 		for line in lines:
 			DetailsAndId=line.strip().split(sp)
-			list.append(DetailsAndId[0])
+			list.append(DetailsAndId[col])
 			idNum+=1
 	return idNum,list
-
+#返回训练样本数和列表
 def openTrain(dir,sp=','):
 	num=0
 	list=[]
@@ -69,5 +94,17 @@ def openTrain(dir,sp=','):
 
 if __name__ == '__main__':
 	dirEntity="data\\people.csv"
-	a,b=openDetailsAndId(dirEntity)
-	print(a,b)
+	entityIdNum,entityList=openDetailsAndId(dirEntity,',',1)
+	print(entityIdNum,entityList)
+	dirRelation = "data\\rel.csv"
+	relationIdNum, relationList = openDetailsAndId(dirRelation,',',1)
+	print(relationIdNum,relationList)
+	#dirTrain = data\\train.txt"
+	#tripleNum, tripleList = openTrain(dirTrain)
+	print("打开TransE")
+	#transE = TransE(entityList,relationList,tripleList, margin=1, dim = 100)
+	#print("TranE初始化")
+	#transE.initialize()
+	#transE.transE(15000)
+	#transE.writeRelationVector("c:\\relationVector.txt")
+	#transE.writeEntilyVector("c:\\entityVector.txt")
